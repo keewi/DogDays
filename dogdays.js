1999f638cdfdb90f032f0f04d9f5453d594b08d5
Emily Yeh <emily@index.com>
2016-11-06 17:59:15 -0800

    toggle page

--- dogdays.js
+++ dogdays.js
@@ -6,6 +6,8 @@ var getAllCallback = function(list) {
 	// TODO: Something Awesome
 };
 
+var expanded = false;
+
 function get_time() {
 	var d = new Date();
 	hours = d.getHours() < 13 ? d.getHours() : d.getHours() - 12;
@@ -25,10 +27,33 @@ function get_date() {
 	return day + ", " + month + " " + d.getDate();
 };
 
+function toggle_page() {
+	console.log("scrolling");
+	if (expanded) {
+		$("#dog-name").animate({top: "80%"});
+		$("#time").fadeIn(600);
+		$("#date").fadeIn(600);
+		$(".search").fadeIn(600);
+		setTimeout(function() {
+			$("#name-btn").html("Learn more...");
+		}, 100);
+	} else {
+		$("#time").fadeOut(300);
+		$("#date").fadeOut(300);
+		$(".search").fadeOut(300);
+		$("#dog-name").animate({top: "50px"});
+		setTimeout(function() {
+			$("#name-btn").html("Click to close");
+		}, 100);
+	}
+	expanded = !expanded;
+};
+
 window.onload = function() {
 	document.getElementById("time").innerHTML = get_time();
 	document.getElementById("date").innerHTML = get_date();
 	setInterval (function() {
 		document.getElementById ('time').firstChild.data = get_time();
 	}, 1000);
-}
+	document.getElementById("dog-name").onclick = function() { toggle_page() };
+};
